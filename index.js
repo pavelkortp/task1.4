d7368e323e23695f5207bfe7f27bbcc08ff89177
Pavlo Zhurbytskyi <92216297+pavelkortp@users.noreply.github.com>
2023-10-19 02:26:46 +0300

    Task2: realise switch btn which can on/off visibility

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ document.getElementById('js-btn').onclick = () => {
 document.getElementById('css-js-btn').onclick = () => {
     document.getElementById('first-square').setAttribute('class', 'hidden');
 }
+
+document.getElementById('switch-btn').onclick = () => {
+    const square = document.getElementById('first-square');
+    console.log('SWITCH');
+    if(square.style.visibility == 'hidden'){
+        square.style.visibility = 'visible';
+    }else{
+        square.style.visibility = 'hidden'
+    }
+}
