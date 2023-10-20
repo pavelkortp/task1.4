8607e6a5d31b0c1cdfb03fef86c14f8a6444a9dc
Pavlo <pavelkortp@gmail.com>
2023-10-20 15:38:28 +0300

    Complete 3 task and start task 4

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 document.getElementById('js-btn').onclick = () => {
-    document.getElementById('first-square').remove();
+    const squares = document.getElementsByClassName('square');
+    for (let i = squares.length-1; i >=0; i--) {
+        squares.item(i).remove();
+    }
 }
 
 document.getElementById('css-js-btn').onclick = () => {
-    document.getElementById('first-square').setAttribute('class', 'hidden');
+    const squares = document.getElementsByClassName('square');
+    for (let i = squares.length-1; i >=0; i--) {
+        squares.item(i).setAttribute('class', 'hidden');
+    }
+
+
 }
 
 document.getElementById('switch-btn').onclick = () => {
-    const square = document.getElementById('first-square');
-    console.log('SWITCH');
-    if(square.style.visibility == 'hidden'){
-        square.style.visibility = 'visible';
-    }else{
-        square.style.visibility = 'hidden'
+    const squares = document.getElementsByClassName('square');
+    for (let i = 0; i < squares.length; i++) {
+        if (squares.item(i).style.visibility == 'hidden') {
+            squares.item(i).style.visibility = 'visible';
+        } else {
+            squares.item(i).style.visibility = 'hidden'
+        }
     }
 }
